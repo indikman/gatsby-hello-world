507dbbe39193be375d44ea90b6a7794cf49a491b
Indika Wijesooriya <indikman@gmail.com>
2020-10-14 08:41:25 +0530

    Learning components and props

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function Contact() {
+  return (
+    <div>
+      <Header headerText="Contact" />
+      <p>Contact me somehow</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style = {
       {color: `purple`}
     }>
-      <h1>Hello Indika!</h1>
+      <Header headerText="Welcome"/>
       <p>What a world!</p>
       <img src= "https://source.unsplash.com/random/400x200" alt=""/>
     </div>
